5b2da084d14cdf3bc036b52927ec20922feece77
Orvin <orvin.tritama@hotmail.com>
2022-09-15 20:50:57 -0700

    added simple uses of both classes in main class

--- Exercise.java
+++ Exercise.java
@@ -1,5 +1,11 @@
 public class Exercise {
     public static void main(String[] args) {
+        String wordToCheck = "racecar";
+        boolean isWordPalindrome = PalindromeStringChecker.isPalindrome(wordToCheck);
+        System.out.println("Word palindrome? T/F? " + isWordPalindrome);
 
+        int numberToCheck = 112211;
+        boolean isIntPalindrome = PalindromeIntegerChecker.isPalindrome(numberToCheck);
+        System.out.println("Integer palindrome? T/F? " + isIntPalindrome );
     }
 }
