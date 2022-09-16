a5943e0a8913ca2db9189d503e7a181140ed2bac
Orvin <orvin.tritama@hotmail.com>
2022-09-15 20:47:05 -0700

    added palindrome int checker and its test cases

---
+++ PalindromeIntegerChecker.java
@@ -0,0 +1,23 @@
+public class PalindromeIntegerChecker {
+
+    public static boolean isPalindrome(int toCheck) {
+        int original = toCheck;
+        int reversed = 0;
+        
+        //if it's negative, convert to positive;
+        if (toCheck < 0) {
+            toCheck *= -1;
+        } else {
+            while (toCheck != 0) {
+                int r = toCheck % 10; // take the last digit
+                toCheck /= 10; // remove the last digit
+                reversed = reversed * 10 + r; // make an extra space at the end, and at remainder
+                 
+            }
+        }
+        
+        // true if original is the same as reversed
+        return (original == reversed) ? true : false;
+    }
+
+}
--- PalindromeStringChecker.java
+++ PalindromeStringChecker.java
@@ -1,4 +1,5 @@
 public class PalindromeStringChecker {
+    
     public static boolean isPalindrome(String toCheck) {
         for(int i = 0, j= toCheck.length() - 1; i < j ; i++ , j--) {
             if (toCheck.charAt(i) != toCheck.charAt(j)) {
